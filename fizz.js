496748ab1a101e2c7cc07ad6c688df70464ac9da
Use your GitHub sernameDIAMONDTDAD <Enter your email address>
2022-02-24 17:41:27 -0400

    g

---
+++ fizz.js
@@ -0,0 +1,13 @@
+for(var i=1;i<=100;i++){
+    if (i % 5 == 0 && i%3==0) {
+        document.writeln("FizzBuzz"+"<br>")
+    }
+    else if (i % 3 == 0){
+        document.writeln("Fizz"+"<br>");
+    } 
+    else if (i % 5 == 0) {
+       document.writeln("Buzz"+"<br>");
+       
+    }
+    else document.writeln(i+"<br>");
+}
