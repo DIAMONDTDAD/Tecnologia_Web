aab140f8f2acf87d15f08097f2cf8504522a0f23
Use your GitHub sernameDIAMONDTDAD <Enter your email address>
2022-03-08 08:14:11 -0400

    k

--- autenticar.js
+++ autenticar.js
@@ -11,3 +11,17 @@ open.addEventListener('click', () => {
 close.addEventListener('click', () => {
   modal_container.classList.remove('show');
 });
+
+function fechaactual(){
+  var fecha =new Date();
+   month = fecha.getUTCMonth()+1;
+   day = fecha.getUTCDate();
+   year = fecha.getUTCFullYear();
+   minutes= fecha.getUTCMinutes();
+   horas= fecha.getUTCHours()-4;
+   seconds= fecha.getUTCSeconds();
+  
+  imprimirDatos= "Entregado el "+month+"/"+day+"/"+year+" a horas : "+horas+":"+minutes+":"+seconds;
+    
+  document.getElementById('result').innerHTML=imprimirDatos;
+}   
